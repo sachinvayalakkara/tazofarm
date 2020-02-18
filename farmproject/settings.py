b4586f4e1055ee7d6980833c0a891ea9a4aef696
sachin <sachinvayalakkara@gmail.com>
2020-02-18 10:04:19 +0530

    initialcommit

--- farmproject/settings.py
+++ farmproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f)*)vm=a68(7koysjle1_7t72jeg36jj5k^8emjl-qs2iuauz6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['farmproject-tazo.herokuapp.com','127.0.0.1']
 
 
 # Application definition
