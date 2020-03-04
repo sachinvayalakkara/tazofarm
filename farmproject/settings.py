3e25d9ebdba42fba6cd0e2ff009188dcba82b10c
sachin <sachinvayalakkara@gmail.com>
2020-03-04 09:54:10 +0530

    for deploy

--- farmproject/settings.py
+++ farmproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f)*)vm=a68(7koysjle1_7t72jeg36jj5k^8emjl-qs2iuauz6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['farmproject-tazo.herokuapp.com','127.0.0.1']
 
