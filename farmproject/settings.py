c7cde34ffe50243b0c0e0e02d32f71498b9e0acc
sachin <sachinvayalakkara@gmail.com>
2020-03-04 17:22:44 +0530

    for deployment

--- farmproject/settings.py
+++ farmproject/settings.py
@@ -134,22 +134,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+
+
+
+
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles') 
 STATIC_URL = '/static/'
 
-
-
-MEDIA_URL ='/media/'
-MEDIA_ROOT =os.path.join(BASE_DIR,'media')
-
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__)) 
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles') 
  
 # Extra lookup directories for collectstatic to find static files 
 STATICFILES_DIRS = ( 
-    os.path.join(PROJECT_ROOT, 'static'), 
+    os.path.join(BASE_DIR, 'static'), 
 ) 
  
 #  Add configuration for static files storage using whitenoise 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
+MEDIA_URL ='/media/'
+MEDIA_ROOT =os.path.join(BASE_DIR,'media')
