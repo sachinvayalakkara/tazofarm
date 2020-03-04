aac2eac9db150dbe42601adbe1d30d17fa35fedf
sachin <sachinvayalakkara@gmail.com>
2020-03-04 11:20:28 +0530

    for deployment

--- farmproject/settings.py
+++ farmproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import djago_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,5 +140,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL ='/media/'
 MEDIA_ROOT =os.path.join(BASE_DIR,'media')
 
-django_heroku.settings(locals())
+
 
