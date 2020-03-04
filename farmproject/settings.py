533426adfc39d8b66eff248d5137bd2455d3a44b
sachin <sachinvayalakkara@gmail.com>
2020-03-04 15:50:07 +0530

    for deployment

--- farmproject/settings.py
+++ farmproject/settings.py
@@ -141,7 +141,7 @@ MEDIA_URL ='/media/'
 MEDIA_ROOT =os.path.join(BASE_DIR,'media')
 
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__)) 
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles') 
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'static') 
  
 # Extra lookup directories for collectstatic to find static files 
 STATICFILES_DIRS = ( 
