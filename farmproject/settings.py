3a80dbb40f304a0e9b8eefe55e422a5df834435e
sachin <sachinvayalakkara@gmail.com>
2020-03-04 11:51:37 +0530

    for deployment

--- farmproject/settings.py
+++ farmproject/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         # 'USER': 'root',
         # 'PASSWORD': '',
         # 'HOST': 'localhost',
-        'PORT' :'3306' ,  
+        'PORT' :'3306' ,
         # 'OPTIONS': {
         #  'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
        
@@ -129,18 +129,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
 MEDIA_URL ='/media/'
 MEDIA_ROOT =os.path.join(BASE_DIR,'media')
